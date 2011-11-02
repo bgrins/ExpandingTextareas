a2fa0a0f2704a859df52f21596d03255a183abb3
Randy Merrill <Zoramite@gmail.com>
2011-11-02 11:33:59 -0700

    Adding the `destroy` functionality.
    Adding a destroy by determining the original values and restoring to the original when destroyed.

--- expanding.js
+++ expanding.js
@@ -20,46 +20,78 @@
     ];
     
     var textareaCSS = {
-        position: "absolute",
-        height: "100%",
-        resize: "none"
+        position : "absolute",
+        height : "100%",
+        resize : "none"
     };
     
     var preCSS = {
-        visibility: "hidden",
-        border: "0 solid"
+        visibility : "hidden",
+        border : "0 solid"
     };
     
     var containerCSS = {
-        position: "relative"
+        position : "relative"
     };
     
     function resize() {
         $(this).parent().find("div").text(this.value + ' ');
     }
-  
-    $.fn.expandingTextarea = function (o) {
+    
+    $.fn.expandingTextarea = function(o) {
         
         if (o === "resize") {
             return this.trigger("input.expanding");
         }
         
-        this.filter("textarea").not(".expanding-init").each(function () {
+        if (o === "destroy") {
+            this.filter(".expanding-init").each(function() {
+                var textarea = $(this);
+                var container = textarea.parents('.expandingText').first();
+                
+                textarea = textarea.detach();
+                
+                textarea
+                    .insertBefore(container)
+                    .css(textarea.data('originalCss') || {})
+                    .attr('style', textarea.data('originalStyle') || '')
+                    .removeClass('expanding-init')
+                    .removeData('originalCss')
+                    .removeData('originalStyle');
+                
+                container.remove();
+            });
             
+            return this;
+        }
+        
+        this.filter("textarea").not(".expanding-init").each(function() {
             var textarea = $(this).addClass("expanding-init");
-
+            
+            // Store the original styles for destroying
+            textarea.data('originalStyle', textarea.attr('style'));
+            
             textarea.wrap("<div class='expandingText'></div>");
             textarea.after("<pre class='textareaClone'><div></div></pre>");
             
             var container = textarea.parent().css(containerCSS);
             var pre = container.find("pre").css(preCSS);
-
+            
+            var originalCSS = {};
+            
+            $.each(textareaCSS, function(i, p) {
+                originalCSS[i] = textarea.css(p);
+            });
+            
+            textarea.data('originalCss', originalCSS);
+            
             textarea.css(textareaCSS);
             
-            $.each(cloneCSSProperties, function (i, p) {
+            $.each(cloneCSSProperties, function(i, p) {
                 var val = textarea.css(p);
                 
-                // Only set if different to prevent overriding percentage css values
+                // Only set if different to prevent overriding percentage css
+                // values
                 if (pre.css(p) !== val) {
                     pre.css(p, val);
                 }
@@ -71,12 +103,11 @@
         
         return this;
     };
-
+    
     $(function () {
         if ($.expandingTextarea.autoInitialize) {
             $($.expandingTextarea.initialSelector).expandingTextarea();
         }
     });
-
+    
 })(jQuery);
-

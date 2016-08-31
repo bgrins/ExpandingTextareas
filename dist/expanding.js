5a2d52af77b474a93e1984c2981e3dbc91d0410e
Dom Christie <christiedom@gmail.com>
2016-08-31 20:19:13 +0100

    Make resetStyles private

--- dist/expanding.js
+++ dist/expanding.js
@@ -184,6 +184,8 @@ var Expanding = function (textarea) {
   this.textarea = new Textarea(textarea);
   this.textareaClone = new TextareaClone();
   this.textarea.oldStyleAttribute = textarea.getAttribute('style');
+  resetStyles.call(this);
+  this.setStyles();
 
   this.wrapper = document.createElement('div');
   this.wrapper.className = 'expanding-wrapper';
@@ -195,7 +197,6 @@ var Expanding = function (textarea) {
   this.wrapper.appendChild(this.textareaClone.element);
 
   this.attach();
-  this.setStyles();
   this.update();
 };
 
@@ -219,6 +220,11 @@ Expanding.prototype = {
     }
   },
 
+  setStyles: function () {
+    this.textareaClone.style(this.textareaClone.styles(this.textarea.element));
+    this.textarea.style(this.textarea.styles());
+  },
+
   // Updates the clone with the textarea value
   update: function () {
     this.textareaClone.value(this.textarea.value());
@@ -232,41 +238,24 @@ Expanding.prototype = {
     this.wrapper.parentNode.insertBefore(this.textarea.element, this.wrapper);
     this.wrapper.parentNode.removeChild(this.wrapper);
     this.textarea.destroy();
-  },
-
-  setStyles: function () {
-    this._resetStyles();
-    this._setCloneStyles();
-    this._setTextareaStyles();
-  },
-
-  // Applies reset styles to the textarea and clone
-  // Stores the original textarea styles in case of destroying
-  _resetStyles: function () {
-    var resetStyles = {
-      margin: 0,
-      webkitBoxSizing: 'border-box',
-      mozBoxSizing: 'border-box',
-      boxSizing: 'border-box',
-      width: '100%'
-    };
-    // Should only be called once i.e. on initialization
-    this.textareaClone.style({
-      minHeight: this.textarea.element.offsetHeight + 'px'
-    });
-    this.textareaClone.style(resetStyles);
-    this.textarea.style(resetStyles);
-  },
-
-  // Sets the basic clone styles and copies styles over from the textarea
-  _setCloneStyles: function () {
-    this.textareaClone.style(this.textareaClone.styles(this.textarea.element));
-  },
-
-  _setTextareaStyles: function () {
-    this.textarea.style(this.textarea.styles());
   }
 };
+
+function resetStyles () {
+  var styles = {
+    margin: 0,
+    webkitBoxSizing: 'border-box',
+    mozBoxSizing: 'border-box',
+    boxSizing: 'border-box',
+    width: '100%'
+  };
+  // Should only be called once i.e. on initialization
+  this.textareaClone.style({
+    minHeight: this.textarea.element.offsetHeight + 'px'
+  });
+  this.textareaClone.style(styles);
+  this.textarea.style(styles);
+}
 
 // Plugin Definition
 // =================
